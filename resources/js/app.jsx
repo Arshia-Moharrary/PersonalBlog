ca15b6aa70a14d4bea45e72a395019c0e26b4f5e
Arshia Moharrary <arshia.moharrary@gmail.com>
2025-05-06 22:16:55 +0330

    app layout created

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,10 +1,16 @@
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
+import AppLayout from './Layouts/AppLayout'
 
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-        return pages[`./Pages/${name}.jsx`]
+        let page = pages[`./Pages/${name}.jsx`].default;
+        
+        page.layout ??= (page) => <AppLayout>{page}</AppLayout>;
+
+        return page;
+
     },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />)

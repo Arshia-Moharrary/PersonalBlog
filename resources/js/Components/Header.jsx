a053d7b49429439bdda8c1d2bd6b47b2846084af
Arshia Moharrary <arshia.moharrary@gmail.com>
2025-05-06 23:42:15 +0330

    header changed

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -2,10 +2,10 @@ export default function Header() {
     return (
         <header className="bg-white shadow-sm">
             <nav className="max-w-6xl mx-auto px-4 py-4 flex justify-center space-x-6 text-sm text-gray-600">
-                <a href="" className="hover:text-black transition">Home</a>
-                <a href="" className="hover:text-black transition">Blog</a>
-                <a href="" className="hover:text-black transition">About</a>
-                <a href="" className="hover:text-black transition">Contact</a>
+                <a href="" className="hover:text-[var(--primary-color)] transition">Home</a>
+                <a href="" className="hover:text-[var(--primary-color)] transition">Blog</a>
+                <a href="" className="hover:text-[var(--primary-color)] transition">About</a>
+                <a href="" className="hover:text-[var(--primary-color)] transition">Contact</a>
             </nav>
         </header>
     )

cb8ee3ae80d854cf11e786073cd41b8aa2e403d0
Arshia Moharrary <arshia.moharrary@gmail.com>
2025-05-06 23:41:05 +0330

    change in post box

--- resources/js/Components/PostBox.jsx
+++ resources/js/Components/PostBox.jsx
@@ -6,7 +6,7 @@ export default function PostBox({ title = 'Posts', id = '', posts = [] }) {
 
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     {posts.map((post, index) => (
-                        <div className="border border-gray-300 p-6 shadow-sm hover:shadow-md transition">
+                        <div key={index} className="border border-gray-300 hover:border-[var(--primary-color)] cursor-pointer p-6 shadow-sm hover:shadow-md transition">
                             <h3 className="text-xl font-semibold mb-2">{post.title}</h3>
                             <p className="text-sm text-gray-600 mb-4">
                                 {post.description}
